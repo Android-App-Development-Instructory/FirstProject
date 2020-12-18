74e8ed7a6aefb3589f83a6801a0390a5e21b4ac3
User <https://github.com/karno786/Sqlite_git_test>
2020-12-19 02:06:34 +0600

    01. Create JAVA Project & Explain Basic - Android Development Course Basic To Advanced . Create a new JAVA Project Explain CODE , Run First Project Methods, Keywords, Statements, Escape Sequence , Comment, short-cut

---
+++ src/firstproject/FirstProject.java
@@ -0,0 +1,37 @@
+
+package firstproject;
+
+
+/*
+   This is a Multiple Line Comment
+   
+    Project Details 
+  --- MD. AL-Amin
+   
+
+*/
+
+
+public class FirstProject {
+
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello Java"); // this is a output line in java
+        
+        
+        System.out.println("\'Hello Programming\'"); // Escape Sequence single quart
+        
+        System.out.println("1 \t2 \t3"); // Escape Sequence tab
+        
+        System.out.println("\"Hello Programming\""); // Escape Sequence double quart
+        
+        System.out.println("Hello \nProgramming"); // Escape Sequence new line
+        
+        
+    }
+    
+    
+
+}
